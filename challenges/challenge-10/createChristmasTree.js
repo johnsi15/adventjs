7d286825edc9367dd2b448e4da6ca0e75b97e261
John Serrano <andrey_201015@hotmail.com>
2023-12-13 18:45:46 -0500

    Add solution challenge 10 v2

--- challenges/challenge-10/createChristmasTree.js
+++ challenges/challenge-10/createChristmasTree.js
@@ -1,24 +1,12 @@
 export function createChristmasTree(ornaments, height) {
   let result = ''
-  const length = ornaments.length // 3
-
-  let duplicate = ornaments.repeat(2).split('')
-  // 1 2 3 1 2 3
-  // position 1 = 1
-  // position 2 = 2 3
-  // position 3 = 1 2 3
-  // position 4 = 1 2 3 1
-
-  // console.log({ duplicate }) // [ '1', '2', '3', '1', '2', '3' ]
+  ornaments = ornaments.repeat(height).split('')
 
   for (let i = 0; i < height; i++) {
-    const corte = duplicate.splice(0, i + 1) // Extraer elementos
-    console.log({ i, duplicate, corte })
+    const corte = ornaments.splice(0, i + 1)
+    result += ' '.repeat(height - i - 1) + corte.join(' ') + '\n'
 
-    const line = ' '.repeat(height - i - 1) + corte.join(' ') + '\n'
-    result += line
-
-    duplicate.push(...corte) // Agregar elementos al final
+    ornaments.push(...corte)
   }
 
   return result + ' '.repeat(height - 1) + '|' + '\n'
@@ -30,11 +18,5 @@ console.log(result)
 const result2 = createChristmasTree('x', 3)
 console.log(result2)
 
-// const result3 = createChristmasTree('xo', 4)
-// console.log(result3)
-// "   x
-//   o x
-//  o x o
-// x o x o
-//    |
-// "
+const result3 = createChristmasTree('xo', 4)
+console.log(result3)
