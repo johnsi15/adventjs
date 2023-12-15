5fc4f858f17d08ddb98dddbd461bec4a3a778476
John Serrano <andrey_201015@hotmail.com>
2023-12-15 12:31:20 -0500

    Solution challenge 11 v2

--- challenges/challenge-11/getIndexsForPalindrome.js
+++ challenges/challenge-11/getIndexsForPalindrome.js
@@ -1,5 +1,25 @@
 export function getIndexsForPalindrome(word) {
-  return [0, 0]
+  const wordList = word.split('')
+  const wordReverse = [...wordList].reverse().join('')
+  if (word === wordReverse) return []
+
+  let wordGood = ''
+  for (let i = 0; i < word.length; i++) {
+    for (let j = i + 1; j < word.length; j++) {
+      const words = [...wordList]
+      const wordNext = words[j]
+      const wordNow = words[i]
+      // console.log({ wordNow, wordNext })
+      words[i] = wordNext // b
+      words[j] = wordNow // a
+      wordGood = words.join('')
+      const wordReverseGood = [...words].reverse().join('')
+      // console.log({ wordReverseGood, wordGood })
+      if (wordReverseGood === wordGood) return [i, j]
+    }
+  }
+
+  return null
 }
 
 const result = getIndexsForPalindrome('anna')
@@ -10,3 +30,6 @@ console.log(result2)
 
 const result3 = getIndexsForPalindrome('abac')
 console.log(result3)
+
+const result4 = getIndexsForPalindrome('aaababa')
+console.log(result4)
