fc3a3a7961e68d0f9590536126eac9af23e48aba
John Serrano <andrey_201015@hotmail.com>
2023-12-18 16:21:24 -0500

    Add solution challenge 12 v2

--- challenges/challenge-12/checkIsValidCopy.js
+++ challenges/challenge-12/checkIsValidCopy.js
@@ -1,8 +1,24 @@
 export function checkIsValidCopy(original, copy) {
+  for (let [i, char] of original.split('').entries()) {
+    const charValid = /([A-Z])|([a-z])|(\#)|(\+)|(\:)|(\.)|(\s)/g
+    const simbolos = [char, char.toLowerCase(), '#', '+', ':', '.', ' ', '']
+
+    const res = simbolos.slice((charValid.exec(char)?.lastIndexOf(char) ?? 1) - 1).includes(copy[i])
+
+    if (!res) return false
+  }
+
   return true
 }
 
-checkIsValidCopy('Santa Claus is coming', 'sa#ta Cl#us i+ comin#') // true
-checkIsValidCopy('s#nta Cla#s is coming', 'p#nt: cla#s #s c+min#') // false (por la p inicial)
-checkIsValidCopy('Santa Claus', 's#+:. c:. s') // true
-checkIsValidCopy('Santa Claus', 's#+:.#c:. s') // false (hay un # donde no debería)
+const result = checkIsValidCopy('Santa Claus is coming', 'sa#ta Cl#us i+ comin#') // true
+console.log({ result })
+// const result2 = checkIsValidCopy('s#nta Cla#s is coming', 'p#nt: cla#s #s c+min#') // false (por la p inicial)
+// console.log({ result2 })
+// const result3 = checkIsValidCopy('Santa Claus', 's#+:. c:. s') // true
+// console.log({ result3 })
+// const result4 = checkIsValidCopy('Santa Claus', 's#+:.#c:. s') // false (hay un # donde no debería)
+// console.log({ result4 })
+
+// const result = checkIsValidCopy('s+#:.#c:. s', 's#+:.#c:. s') // false
+// console.log(result)
