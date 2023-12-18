ae42439f9026862669df66633773fb6910d33dc9
John Serrano <andrey_201015@hotmail.com>
2023-12-18 18:32:25 -0500

    Add test challenge 12

--- challenges/challenge-12/test/checkIsValidCopy.test.js
+++ challenges/challenge-12/test/checkIsValidCopy.test.js
@@ -1,12 +1,32 @@
 import { checkIsValidCopy } from '../checkIsValidCopy.js'
-describe('Ordenando el almacén', () => {
+describe('¿Es una copia válida?', () => {
   test('Test #1', () => {
-    const word = 'anna'
-    const result = checkIsValidCopy(word)
+    const result = checkIsValidCopy('Santa Claus is coming', 'sa#ta Cl#us i+ comin#')
 
-    const SOLUTION = []
+    expect(result).toBeTruthy()
+  })
 
-    expect(result).toBeDefined()
-    expect(result).toStrictEqual(SOLUTION)
+  test('Test #2', () => {
+    const result = checkIsValidCopy('s#nta Cla#s is coming', 'p#nt: cla#s #s c+min#')
+
+    expect(result).toBeFalsy()
+  })
+
+  test('Test #3', () => {
+    const result = checkIsValidCopy('Santa Claus', 's#+:.#c:. s')
+
+    expect(result).toBeFalsy()
+  })
+
+  test('Test #4', () => {
+    const result = checkIsValidCopy('s+#:.#c:. s', 's#+:.#c:. s')
+
+    expect(result).toBeFalsy()
+  })
+
+  test('Test #5', () => {
+    const result = checkIsValidCopy('Santa Claus', 's#+:. c:. s')
+
+    expect(result).toBeTruthy()
   })
 })
