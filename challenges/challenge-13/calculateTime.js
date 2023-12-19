7f376a596816a33964c7b8efc639b22b67e34ef9
John Serrano <andrey_201015@hotmail.com>
2023-12-19 16:43:01 -0500

    Add solution challenge 13 v2

--- challenges/challenge-13/calculateTime.js
+++ challenges/challenge-13/calculateTime.js
@@ -1,54 +1,74 @@
 export function calculateTime(deliveries) {
-  // const startTime = '00:00:00' // HH:mm:ss
   let limitTime = '07:00:00' // HH:mm:ss
 
-  // let timeTotal = ''
   let hoursTotal = 0
   let minutesTotal = 0
   let secondsTotal = 0
 
-  // 06:50:00
-  // 05:50:00
-  // -02:20:00
-
   for (const delivery of deliveries) {
-    console.log(delivery)
+    // console.log(delivery)
     const hours = Number(delivery.split(':')[0])
     const minutes = Number(delivery.split(':')[1])
     const seconds = Number(delivery.split(':')[2])
-
-    const limitHours = Number(limitTime.split(':')[0])
-    const limitMinutes = Number(limitTime.split(':')[1])
-    const limitSeconds = Number(limitTime.split(':')[2])
-
-    // console.log({ hoursTotal, limitHours, hours })
-    hoursTotal = hours === 7 ? hours : limitHours - hours
-    secondsTotal = limitSeconds - seconds === 0 ? `00` : limitSeconds - seconds
-
-    if (limitMinutes === 0) {
-      minutesTotal = 60 - minutes
-      hoursTotal = hoursTotal - 1
-    } else {
-      minutesTotal = limitMinutes - minutes
-    }
-
-    // if (hoursTotal === 0) {
-    //   hoursTotal = 24
-    // }
-
-    limitTime = `0${hoursTotal}:${minutesTotal}:${secondsTotal}`
-    // console.log({ hoursTotal, minutesTotal, secondsTotal })
+    console.log({ hours })
+    hoursTotal += hours
+    minutesTotal += minutes
+    secondsTotal += seconds
   }
 
-  console.log(limitTime)
+  if (secondsTotal >= 60) {
+    secondsTotal = secondsTotal - 60
+    minutesTotal++
+  }
 
-  return '' // HH:mm:ss
+  if (minutesTotal >= 60) {
+    minutesTotal = minutesTotal - 60
+    hoursTotal++
+  }
+
+  console.log({ hoursTotal, minutesTotal, secondsTotal })
+
+  let limitHours = Number(limitTime.split(':')[0])
+  let limitMinutes = Number(limitTime.split(':')[1])
+  let limitSeconds = Number(limitTime.split(':')[2])
+
+  hoursTotal = limitHours - hoursTotal
+  minutesTotal = limitMinutes - minutesTotal
+  secondsTotal = limitSeconds - secondsTotal
+  console.log(limitMinutes, minutesTotal)
+
+  if (secondsTotal < 0) {
+    secondsTotal = 60 + secondsTotal
+    minutesTotal--
+  }
+
+  if (minutesTotal < 0) {
+    minutesTotal = 60 + minutesTotal
+    hoursTotal--
+  }
+  console.log({ hoursTotal, minutesTotal, secondsTotal })
+
+  if (minutesTotal <= 0 && hoursTotal <= 0 && secondsTotal <= 0) {
+    return '00:00:00'
+  }
+
+  hoursTotal = hoursTotal < 0 ? '00' : `-0${hoursTotal}`
+  minutesTotal = minutesTotal < 0 ? '00' : minutesTotal
+
+  return `${hoursTotal}:${minutesTotal}:${secondsTotal}0`
 }
 
-const result = calculateTime(['00:10:00', '01:00:00', '03:30:00'])
-// '-02:20:00'
-console.log({ result })
+// const result = calculateTime(['00:10:00', '01:00:00', '03:30:00'])
+// // '-02:20:00'
+// console.log({ result })
 
 // const result2 = calculateTime(['02:00:00', '05:00:00', '00:30:00'])
 // // '00:30:00'
 // console.log({ result2 })
+
+// const result3 = calculateTime(['00:45:00', '00:45:00', '00:00:30', '00:00:30']) // '-05:29:00'
+// console.log({ result3 })
+
+const result4 = calculateTime(['01:01:01', '09:59:59', '01:01:01'])
+// "05:02:01"
+console.log({ result4 })
