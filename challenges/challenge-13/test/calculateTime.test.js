0bd4395f9634d064085bdb25c877874ef28c2d53
John Serrano <andrey_201015@hotmail.com>
2023-12-20 09:55:43 -0500

    Add test challenge 13

--- challenges/challenge-13/test/calculateTime.test.js
+++ challenges/challenge-13/test/calculateTime.test.js
@@ -2,7 +2,17 @@ import { calculateTime } from '../calculateTime.js'
 describe('Calculando el tiempo', () => {
   test('Test #1', () => {
     const result = calculateTime(['00:10:00', '01:00:00', '03:30:00'])
+    const SOLUTION = '-02:20:00'
 
-    expect(result).toBeTruthy()
+    expect(result).toBeDefined()
+    expect(result).toBe(SOLUTION)
+  })
+
+  test('Test #2', () => {
+    const result = calculateTime(['02:00:00', '05:00:00', '00:30:00'])
+    const SOLUTION = '00:30:00'
+
+    expect(result).toBeDefined()
+    expect(result).toBe(SOLUTION)
   })
 })
