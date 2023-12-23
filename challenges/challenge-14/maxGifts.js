ca28914aa27dfd6869b214139b339b815d44e4cc
John Serrano <andrey_201015@hotmail.com>
2023-12-23 12:23:15 -0500

    Add solution challenge 14 v2

--- challenges/challenge-14/maxGifts.js
+++ challenges/challenge-14/maxGifts.js
@@ -1,37 +1,34 @@
 export function maxGifts(houses) {
-  console.log(houses)
   const len = houses.length
+  let maxValue = 0
 
-  let maxValue = Math.max(...houses)
-  console.log({ maxValue })
+  if (len === 1) {
+    return houses[0]
+  }
 
-  for (let i = 1; i <= len; i++) {
-    console.log({ i, gifts: houses[i] })
-    if (len === 3 && i === 1) {
-      console.log('paso -> ' + i + ' - ' + houses[i - 1])
-      maxValue += houses[i - 1]
-    }
+  let prevMax = houses[0]
+  maxValue = Math.max(houses[0], houses[1])
 
-    if (i > 3 && i % 2 === 0) {
-      console.log('paso -> ' + i + ' - ' + houses[i - 1])
-      maxValue += houses[i - 1]
-    }
+  for (let i = 2; i < len; i++) {
+    const temp = maxValue
+    maxValue = Math.max(maxValue, prevMax + houses[i])
+    prevMax = temp
   }
 
   return maxValue
 }
 
-// const result = maxGifts([2, 4, 2]) // 4 (4)
-// console.log({ result })
+const result = maxGifts([2, 4, 2]) // 4 (4)
+console.log({ result })
 
-// const result2 = maxGifts([5, 1, 1, 5]) // 10 (5 + 5)
-// console.log({ result2 })
+const result2 = maxGifts([5, 1, 1, 5]) // 10 (5 + 5)
+console.log({ result2 })
 
-// const result3 = maxGifts([4, 1, 1, 4, 2, 1]) // 9 (4 + 4 + 1)
-// console.log({ result3 })
+const result3 = maxGifts([4, 1, 1, 4, 2, 1]) // 9 (4 + 4 + 1)
+console.log({ result3 })
 
 // const result4 = maxGifts([1, 3, 1, 3, 100]) // 103
 // console.log({ result4 })
 
-const result5 = maxGifts([3, 4, 5]) // maxGifts([1, 1, 1]) // 2
-console.log({ result5 })
+// const result5 = maxGifts([100]) // 100
+// console.log({ result5 })
