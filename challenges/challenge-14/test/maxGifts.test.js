25b7cfae51650cb45120a9e8c9c8880f1aabcad3
John Serrano <andrey_201015@hotmail.com>
2023-12-23 12:24:25 -0500

    Add test challenge 14

--- challenges/challenge-14/test/maxGifts.test.js
+++ challenges/challenge-14/test/maxGifts.test.js
@@ -2,7 +2,15 @@ import { maxGifts } from '../maxGifts.js'
 describe('Calculando el tiempo', () => {
   test('Test #1', () => {
     const result = maxGifts([2, 4, 2])
-    const SOLUTION = '4 (4)'
+    const SOLUTION = 4
+
+    expect(result).toBeDefined()
+    expect(result).toBe(SOLUTION)
+  })
+
+  test('Test #2', () => {
+    const result = maxGifts([5, 1, 1, 5])
+    const SOLUTION = 10
 
     expect(result).toBeDefined()
     expect(result).toBe(SOLUTION)
