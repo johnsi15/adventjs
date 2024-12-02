715ceb44eaf16e3837d39acd1cf69cc2981d0033
John Serrano <andrey_201015@hotmail.com>
2024-12-02 16:55:49 -0500

    changes test challenge 01

--- challenges-2024/challenge-01/test/gifts.test.js
+++ challenges-2024/challenge-01/test/gifts.test.js
@@ -1,10 +1,10 @@
 import { prepareGifts } from '../gifts.js'
 describe('First repeat gift', () => {
   test('Should return an array without duplicates', () => {
-    const giftIds = [2, 1, 3, 5, 3, 2]
+    const giftIds = [3, 1, 2, 3, 4, 2, 5]
     const arrayWithOutDuplicate = prepareGifts(giftIds)
 
     expect(arrayWithOutDuplicate).toBeDefined()
-    expect(arrayWithOutDuplicate).toBe([1, 2, 3, 4, 5])
+    expect(arrayWithOutDuplicate).toEqual([1, 2, 3, 4, 5])
   })
 })
