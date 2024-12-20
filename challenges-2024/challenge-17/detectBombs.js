ea52bceb3ab243fbcea9b9efeb832d881b236a4f
John Serrano <andrey_201015@hotmail.com>
2024-12-20 16:58:33 -0500

    Add v1 solution challenge 17

--- challenges-2024/challenge-17/detectBombs.js
+++ challenges-2024/challenge-17/detectBombs.js
@@ -1,11 +1,103 @@
 export function detectBombs(grid) {
-  // Code here
-  return []
+  const result = []
+  // console.log(grid.length)
+  for (let i = 0; i < grid.length; i++) {
+    const counts = []
+
+    for (let j = 0; j < grid.length; j++) {
+      // const isBorder = (i === 0 || i === grid.length - 1 || j === 0 || j === grid[i].length - 1);
+      const isBorderTop = i === 0
+      const isBorderBottom = i === grid.length - 1
+      const isBorderLeft = j === 0
+      const isBorderRight = j === grid[i].length - 1
+
+      if (isBorderTop && isBorderLeft) {
+        // const crr = grid[i][j]
+        const next = grid[i][j + 1]
+        const bottom = grid[i + 1][j]
+        const diagonal = grid[i + 1][j + 1]
+
+        let count = [next, bottom, diagonal].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderTop && isBorderRight) {
+        // const crr = grid[i][j]
+        const prev = grid[i][j - 1]
+        const bottom = grid[i + 1][j]
+        const diagonal = grid[i + 1][j - 1]
+
+        let count = [prev, bottom, diagonal].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderBottom && isBorderLeft) {
+        // const crr = grid[i][j]
+        const next = grid[i][j + 1]
+        const top = grid[i - 1][j]
+        const diagonal = grid[i - 1][j + 1]
+
+        let count = [next, top, diagonal].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderTop) {
+        // const crr = grid[i][j]
+        const prev = grid[i][j - 1]
+        const next = grid[i][j + 1]
+        const diagonal = grid[i - 1][j - 1]
+
+        let count = [prev, next, diagonal].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderBottom) {
+        // const crr = grid[i][j]
+        const prev = grid[i][j - 1]
+        const next = grid[i][j + 1]
+        const diagonal = grid[i - 1][j + 1]
+
+        let count = [prev, next, diagonal].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderLeft) {
+        // const crr = grid[i][j]
+        const next = grid[i][j + 1]
+        const top = grid[i - 1][j]
+        const bottom = grid[i + 1][j]
+        const diagonal = grid[i + 1][j + 1]
+        const diagonalTop = grid[i - 1][j + 1]
+
+        let count = [next, top, bottom, diagonal, diagonalTop].filter(Boolean).length
+        counts.push(count)
+      } else if (isBorderRight) {
+        // const crr = grid[i][j]
+        const prev = grid[i][j - 1]
+        const top = grid[i - 1][j]
+        const bottom = grid[i + 1][j]
+        const diagonal = grid[i + 1][j - 1]
+        const diagonalTop = grid[i - 1][j - 1]
+
+        let count = [prev, top, bottom, diagonal, diagonalTop].filter(Boolean).length
+        counts.push(count)
+      } else {
+        // const crr = grid[i][j]
+        const next = grid[i][j + 1]
+        const prev = grid[i][j - 1]
+        const bottom = grid[i + 1][j]
+        const top = grid[i - 1][j]
+        const diagonal = grid[i + 1][j + 1]
+        const diagonalTop = grid[i - 1][j + 1]
+        const diagonalBottom = grid[i + 1][j - 1]
+        const diagonalTopLeft = grid[i - 1][j - 1]
+
+        let count = [next, prev, bottom, top, diagonal, diagonalTop, diagonalBottom, diagonalTopLeft].filter(
+          Boolean
+        ).length
+        counts.push(count)
+      }
+      break
+    }
+    console.log(counts)
+
+    // result.push(counts)
+  }
 }
 
 const result = detectBombs([
   [true, false, false],
-  [false, true, false],
+  [false, true, false], // diagonal tiene una bomba
   [false, false, false],
 ])
 
@@ -14,4 +106,13 @@ console.log(result)
 //   [1, 2, 1],
 //   [2, 1, 1],
 //   [1, 1, 1]
+// ]
+
+// detectBombs([
+//   [true, false],
+//   [false, false],
+// ])
+// [
+//   [0, 1],
+//   [1, 1]
 // ]
